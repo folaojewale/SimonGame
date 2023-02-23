d8bfd38feb8ea295317ae29b19e5335c179f3f51
Fola Ojewale <97245615+folaojewale@users.noreply.github.com>
2023-02-23 17:30:17 +0000

    Add files via upload

--- simonSays_function.js
+++ simonSays_function.js
@@ -1,21 +1,23 @@
 
 
 function buttonNumberPressed(){
-    let green = document.querySelector('.green');
-    let blue = document.querySelector('.blue');
-    let red = document.querySelector('.red');
-    let yellow = document.querySelector('.yellow');
-    if(green.addEventListener('click')){
+    document.getElementById("greenButton").onclick = function()
+    {
         return 0;
     }
-    else if(red.addEventListener('click')){
+    document.getElementById("redButton").onclick = function()
+    {
         return 1;
     }
-    else if(yellow.addEventListener('click')){
+
+    document.getElementById("yellowButton").onclick = function()
+    {
         return 2;
     }
-    else
-    return 3;
+    document.getElementById("blueButton").onclick = function()
+    {
+        return 3;
+    }
 }
 
 
@@ -33,17 +35,29 @@ function game(){
         gameSequence[p]= Math.floor(Math.random() * 4);
         var show = gameSequence[i];
         
-        if(show==0){green.}
-        if(show==1){document.getElementById("red")}
-        if(show==2){document.getElementById("yellow")}
-        if(show==3){document.getElementById("blue")}
-        if(!playerAnswer(gameSequence)){
-            gameOver();
-            break;
+        if(show==0){
+            document.getElementById("greenButton").style.backgroundColor="red";
+            setTimeout(time(),3000);
+            document.getElementById("greenButton").style.backgroundColor="rgb(0, 117, 35)";
+            console.log("0");
+            //green.querySelector(".green:active")
         }
-        else if(playerAnswer(gameSequence)){}
-        size++;
-        p++;
+        if(show==1){
+            console.log("1");
+            document.querySelector(".red:active")}
+        if(show==2){
+            console.log("2");
+            document.querySelector(".yellow:active")}
+        if(show==3){
+            console.log("3");
+            document.querySelector(".blue:active")}
+        // if(!playerAnswer(gameSequence)){
+        //     gameOver();
+        //     break;
+        // }
+        //     if(playerAnswer(gameSequence)){}
+        // size++;
+        //p++;
     } 
 }
 function playerAnswer(gameSequence){
@@ -74,7 +88,7 @@ function start() {
     console.log("game has started");
     game();
 
-    setTimeout(time(),3000);
+    //setTimeout(time(),3000);
   }
 
   function time(){
